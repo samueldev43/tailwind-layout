40d50cb985d4abf9fdc8693415921f558063cad9
Samuel <gmc103709@gmail.com>
2024-03-21 12:50:40 +0000

    tailwind project layout building

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Tailwind project",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="bg-zinc-800 text-zinc-50">{children}</body>
+    </html>
+  );
+}
